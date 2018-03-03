16bb4411ccc5f3fb8e0689763c012cbbdfdb5449
jrunestone <johanrunsten86@gmail.com>
2018-03-03 12:11:42 +0100

    Removed deprecated react native method createJSModules (since v47)

--- android/src/main/java/com/reactlibrary/RNTesseractOcrPackage.java
+++ android/src/main/java/com/reactlibrary/RNTesseractOcrPackage.java
@@ -17,11 +17,6 @@ public class RNTesseractOcrPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }

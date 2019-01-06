c70460c220ed29b89249919123bbe94e1431643e
Jonathan Palma <tanpalma04@gmail.com>
2019-01-06 01:19:38 -0600

    Removed tesseractOcrSample before creating some examples using react-native-camera and react-native-image-picker

--- tesseractOcrSample/index.android.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native Tesseract OCR App
- * @author Jonathan Palma <tanpalma04@gmail.com>
- * @flow
- */
-import { AppRegistry } from 'react-native';
-import App from './index.js';
-
-AppRegistry.registerComponent('tesseractOcrSample', () => App);

5b36931d5c0334e7d2b022c5d21afa34a2328247
Jonathan Palma <tanpalma04@gmail.com>
2020-07-03 18:54:50 -0600

    perf(android): improve tesseract setup process
    add 'shouldCopyTrainedFile' to check if tesseract setup is required, and also replace
    `copyTessDataFiles` with `copyTrainedFile` to avoid copying unnecessary files during current
    recognition session

--- android/src/main/java/com/reactlibrary/TesseractOcrModule.java
+++ android/src/main/java/com/reactlibrary/TesseractOcrModule.java
@@ -1,31 +1,45 @@
 package com.reactlibrary;
 
-import android.os.Handler;
-import android.support.annotation.Nullable;
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class TesseractOcrModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
-    private Handler handler = new Handler();
-    private TessBaseAPI tesseract;
-
     private static final String KEY_ALLOW_LIST = "allowlist";
     private static final String KEY_DENY_LIST = "denylist";
-    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + File.separator;
-	  private static final String DATA_FILES_DIRECTORY = "tessdata";
+    private static final String TESS_FILES_DIRECTORY = "tessdata";
+    private static final String TESS_FILES_EXTENSION = ".traineddata";
+    private static String DATA_PATH = Environment.getExternalStorageDirectory().toString();
+    private static String TESS_FILES_PATH;
+    private TessBaseAPI tesseract;
 
     public TesseractOcrModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
         if (!this.DATA_PATH.contains(reactContext.getPackageName())) {
-            this.DATA_PATH += reactContext.getPackageName() + File.separator;
+            this.DATA_PATH += File.separator + reactContext.getPackageName();
         }
+        this.TESS_FILES_PATH = this.DATA_PATH + File.separator + this.TESS_FILES_DIRECTORY;
     }
 
     @Override
@@ -35,26 +49,31 @@ public class TesseractOcrModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void stop(Promise promise) {
-		    Log.d(getName(), "stop");
+        Log.d(getName(), "stop");
+        
         try {
             tesseract.stop();
             tesseract.end();
-            promise.resolve("Recognition was canceled");
+            promise.resolve("Recognition cancelled successfully");
         } catch (Exception e) {
-            Log.e(getName(), e.toString());
-            promise.reject("An error occurred when trying to stop recognition", e.toString());
+            Log.e(getName(), "Could not stop recognition. " + e.toString(), e);
+            promise.reject("Could not stop recognition", e.toString());
         }
     }
 
     @SuppressLint("StaticFieldLeak")
     @ReactMethod
-    public void recognize(String path, String lang, @Nullable ReadableMap options, final Callback callback, final Promise promise) {
+    public void recognize(String imagePath, final String lang, @Nullable ReadableMap tessOptions, final Callback callback, final Promise promise) {
         Log.d(getName(), "recognize");
 
         try {
-            prepareTesseract();
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+            if (shouldCopyTrainedFile(lang)) {
+                prepareTrainedFilesDirectory();
+                copyTrainedFile(lang);
+            }
+
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            final Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
 
             new Thread() {
                 @Override
@@ -62,11 +81,11 @@ public class TesseractOcrModule extends ReactContextBaseJavaModule {
                     tesseract = new TessBaseAPI(new TessBaseAPI.ProgressNotifier() {
                         @Override
                         public void onProgressValues(TessBaseAPI.ProgressValues progressValues) {
-                            Log.d("progress " + progressValues.getPercent().toString());
+                            Log.d(getName(), "progress " + String.valueOf(progressValues.getPercent()));
                             callback.invoke(progressValues.getPercent());
                         }
                     });
-                    tesseract.init(DATA_PATH, lang);
+                    tesseract.init(DATA_PATH + File.separator, lang);
                     tesseract.setImage(bitmap);
                     tesseract.getHOCRText(0);
 
@@ -78,63 +97,55 @@ public class TesseractOcrModule extends ReactContextBaseJavaModule {
 
             }.start();
 
+        } catch (IOException e) {
+            Log.e(getName(), "Could not access trained files. " + e.toString(), e);
+            promise.reject("Could not access trained files", e.toString());
         } catch (Exception e) {
-            Log.e(getName(), e.getMessage());
+            Log.e(getName(), "Could not recognize text. " + e.toString(), e);
             promise.reject("Could not recognize text", e.toString());
         }
     }
 
-    private void prepareDirectory(String path) {
-        Log.d(getName(), "prepare directory");
+    private boolean shouldCopyTrainedFile(String lang) {
+        Log.d(getName(), "should copy trained files?");
 
-        File dir = new File(path);
+        String filePath = TESS_FILES_PATH + File.separator + lang + TESS_FILES_EXTENSION;
+        File file = new File(filePath);
+        return !file.exists();
+    }
+
+    private void prepareTrainedFilesDirectory() throws IOException {
+        Log.d(getName(), "prepare trained files directory");
+
+        File dir = new File(TESS_FILES_PATH);
         if (!dir.exists()) {
             if (!dir.mkdirs()) {
-                Log.e(getName(), "Could not create directory (" + path + "), please make sure the app is allowed to write to external storage.");
+                Log.e(getName(), "Could not create directory, please make sure the app has write permission");
+                throw new IOException("Could not create directory");
             }
-        } else {
-            Log.d(getName(), "Created directory " + path);
         }
     }
 
-    private void prepareTesseract() {
-        Log.d(getName(), "prepare tesseract environment");
+    private void copyTrainedFile(String lang) throws IOException {
+        Log.d(getName(), "copy tesseract data file (" + lang + ")");
 
-        try {
-            prepareDirectory(DATA_PATH + DATA_FILES_DIRECTORY);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        copyTessDataFiles(DATA_FILES_DIRECTORY);
+        String assetPath = TESS_FILES_DIRECTORY + File.separator + lang + TESS_FILES_EXTENSION;
+        String newAssetPath = DATA_PATH + File.separator + assetPath;
+        copyAsset(assetPath, newAssetPath);
     }
 
-    private void copyTessDataFiles(String path) {
-        Log.d(getName(), "copy tesseract data files");
-        try {
-            String fileList[] = reactContext.getAssets().list(path);
+    private void copyAsset(String from, String to) throws IOException {
+        InputStream in = reactContext.getAssets().open(from);
+        OutputStream out = new FileOutputStream(to);
+        byte[] buf = new byte[1024];
+        int len;
 
-            for (String fileName : fileList) {
-                String oldPath = path + File.separator + fileName;
-                String newPath = DATA_PATH + oldPath;
-
-                if (!(new File(newPath)).exists()) {
-                    InputStream in = reactContext.getAssets().open(oldPath);
-                    OutputStream out = new FileOutputStream(newPath);
-                    byte[] buf = new byte[1024];
-                    int len;
-
-                    while ((len = in.read(buf)) > 0) {
-                        out.write(buf, 0, len);
-                    }
-                    in.close();
-                    out.close();
-
-                    Log.d(getName(), "Copied " + oldPath + " to " + newPath);
-                }
-            }
-        } catch (IOException e) {
-            Log.e(getName(), "Unable to copy files to tessdata", e.toString());
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
         }
+        in.close();
+        out.close();
+
+        Log.d(getName(), "Copied " + from + " to " + to);
     }
 }
